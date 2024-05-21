5c2df1ccf1cc2455937c6d37a68d031658f8adae
Louis-Miguel <lpfermiza@up.edu.ph>
2024-05-21 23:31:51 +0800

    added website

---
+++ DeployModel/DeployModel/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import joblib
+
+
+def home(request):
+    return render(request, "home.html")
+
+def result(request):
+    model = joblib.load("model.sav")
+
+    vect = joblib.load("vect.sav")
+    transformed_message  = vect.transform([request.GET["message"]])
+    ans = model.predict(transformed_message)
+    
+    return render(request, "result.html", {"ans": "Scam" if ans == [1] else "Regular Text"})
